8d2b692a7334e2fedde7a2043634fa9fa94c2c05
BjornAlexCoL <75667280+BjornAlexCoL@users.noreply.github.com>
2022-02-16 09:24:27 +0100

    Comments added. Some minor adjustments.

--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Hangman
 {
@@ -6,23 +7,21 @@ namespace Hangman
     {
         static void Main(string[] args)
         {
-            string[] wordList = new string[] { "ANANAS", "AX", "ZWYCKWAX", "MISSISSIPI" };
+            string[] wordList = new string[] { "ANANAS", "AX", "SWIXWAX", "MISSISSIPI" };
             Random rand = new Random();
             string secretWord = wordList[rand.Next(0, wordList.Length)];
-            string displayWord = new string('_', secretWord.Length);
-            char[] displayWordCharArray = displayWord.ToCharArray();
+            char[] displayWordCharArray = new string('_', secretWord.Length).ToCharArray();
             string guessedLetters = "";
             int guesses = 0;
             bool validGuess = false;
             string inpString;
             DisplayHangman(0);
-            Console.WriteLine("{0}",secretWord);
-            Console.WriteLine("{0}",displayWord);
+            Console.WriteLine("{0}", secretWord);
             do
             {
                 DisplayHangman(guesses);
-                Console.WriteLine(displayWord);
-                Console.WriteLine("This is guess number {0}", guesses + 1);
+                Console.WriteLine(displayWordCharArray);
+                Console.WriteLine("\nThis is guess number {0}", guesses + 1);
                 if (guessedLetters.Length > 0)
                 {
                     Console.WriteLine("You have guessed on letters {0} ", guessedLetters);
@@ -33,25 +32,24 @@ namespace Hangman
                 validGuess = true;
                 if (inpString.Length == 1)
                 {
-                    if (!guessedLetters.Contains(inpString))
+                    if (!guessedLetters.Contains(inpString))//Already guessed that letter
                     {
                         validGuess = true;
-                        guessedLetters+=inpString+",";
-                        for (int testLoop = 0; testLoop < secretWord.Length; testLoop++)
+                        guessedLetters += inpString + ",";
+                        for (int testLoop = 0; testLoop < secretWord.Length; testLoop++)//Loop to go through and compare the letter with letters in secret word
                         {
-                            if (secretWord[testLoop] == inpString[0])
+                            if (secretWord[testLoop] == inpString[0])//If letter is in secretword add it to the charray
                             {
-                                
+
                                 displayWordCharArray[testLoop] = inpString[0];
-                                displayWord = string.Concat( displayWordCharArray);
-                                validGuess = false;
+                                validGuess = false;//Don't count guesses.
                             }
 
                         }
                     }
                     else
                     {
-                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Red;//Already guessed message. Fix a delay.
                         Console.WriteLine("You already guessed on that letter!");
                         validGuess = false;
                         Console.ResetColor();
@@ -59,99 +57,91 @@ namespace Hangman
                 }
                 else
                 {
-                    if (inpString == secretWord)
+                    if (inpString == secretWord) //Yay you guessed the word.
                     {
-                        Console.WriteLine("Grattis");
-                        displayWord = inpString;
-                        validGuess = false;
+                        Console.WriteLine("Congratulation");
+                        validGuess = false; //don't count the guess
                     }
                     else
                     {
-                        //Add input to string
-                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.ForegroundColor = ConsoleColor.Red; //Sorry failed guess
                         Console.WriteLine("Sorry wrong word but it was a nice try!");
                         Console.ResetColor();
-                        validGuess = true;
+                        validGuess = true; //count the guess
                     }
                 }
-                if (validGuess)
+                if (validGuess) //increase guess
                 {
                     guesses++;
                 }
-            } while ((secretWord != displayWord) && guesses<=10);
+            } while ((secretWord != string.Concat(displayWordCharArray)) && guesses <= 10);//End when displayWordCharArray is all letters and equal with secret word or guesses reached 10.
         }
 
         private static string getValidLetters(string message)
         {
             char inpKey;
-            string validChars = "abcdefghijklmnopqrstuvwxyzåäöABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ";
+            string validLetters = "abcdefghijklmnopqrstuvwxyzåäöABCDEFGHIJKLMNOPQRSTUVWXYZÅÄÖ";
             string inpString = "";
             Console.Write("{0}: ", message);
             do
             {
                 inpKey = Console.ReadKey(true).KeyChar;
-                if (validChars.Contains(inpKey))
+                if (validLetters.Contains(inpKey)) //Check for valid input
                 {
-                    Console.Write("{0}", inpKey);
+                    Console.Write(inpKey);
                     inpString = inpString + inpKey;
                 }
                 else
                 {
-                    if (inpKey != '\r')
+                    if (inpKey != '\r') //Check for Enter in inkey otherwise wrong input
                     {
-                        Console.WriteLine("I don't think '{0}' is a letter. Try Again", inpKey);
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\nI don't think '{0}' is a letter. Try Again", inpKey);
+                        Console.ResetColor();
                     }
                 }
-            } while (inpKey != '\r' || inpString == "");
+            } while (inpKey != '\r' || inpString == "");//End when valid code otherwise retry.
 
-            return inpString.ToUpper();
+            return inpString.ToUpper(); //Return Uppercase input
         }
 
-        private static void DisplayHangman(int guesses)
+        private static void DisplayHangman(int guesses) //Not needed but more fun ;-)
         {
 
             Console.Clear();
+            Console.ForegroundColor = ConsoleColor.DarkRed;
             switch (guesses)
             {
                 case 0:
                     Console.WriteLine("\n\n\n\n\n");
                     break;
                 case 1:
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("\n\n\n\n\n├──────┐");
-
                     break;
                 case 2:
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("\n|");
                     Console.WriteLine("|");
                     Console.WriteLine("|");
                     Console.WriteLine("|");
                     Console.WriteLine("├──────┐");
-
                     break;
                 case 3:
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("______");
                     Console.WriteLine("|/");
                     Console.WriteLine("|");
                     Console.WriteLine("|");
                     Console.WriteLine("|");
                     Console.WriteLine("├──────┐");
-
                     break;
                 case 4:
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("______");
                     Console.WriteLine("|/   |");
                     Console.WriteLine("|");
                     Console.WriteLine("|");
                     Console.WriteLine("|");
                     Console.WriteLine("├──────┐");
-
                     break;
                 case 5:
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("______");
                     Console.WriteLine("|/   |");
                     Console.WriteLine("|    O");
@@ -160,7 +150,6 @@ namespace Hangman
                     Console.WriteLine("├──────┐");
                     break;
                 case 6:
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("______");
                     Console.WriteLine("|/   |");
                     Console.WriteLine("|    O");
@@ -169,7 +158,6 @@ namespace Hangman
                     Console.WriteLine("├──────┐");
                     break;
                 case 7:
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("______");
                     Console.WriteLine("|/   |");
                     Console.WriteLine("|    O");
@@ -178,7 +166,6 @@ namespace Hangman
                     Console.WriteLine("├──────┐");
                     break;
                 case 8:
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("______");
                     Console.WriteLine("|/   |");
                     Console.WriteLine("|    O");
@@ -187,7 +174,6 @@ namespace Hangman
                     Console.WriteLine("├──────┐");
                     break;
                 case 9:
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("______");
                     Console.WriteLine("|/   |");
                     Console.WriteLine("|    O");
@@ -196,7 +182,6 @@ namespace Hangman
                     Console.WriteLine("├──────┐");
                     break;
                 case 10:
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
                     Console.WriteLine("______");
                     Console.WriteLine("|/   |");
                     Console.WriteLine("|    O");
@@ -205,6 +190,7 @@ namespace Hangman
                     Console.WriteLine("├──────┐");
                     break;
             }
+            Console.WriteLine();
             Console.ResetColor();
         }
 
